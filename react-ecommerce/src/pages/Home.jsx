dab3d1d5bf3e582055c4fe1e1faea218afd7c6c7
epinark <122348695+epinark@users.noreply.github.com>
2024-01-02 20:48:10 +0100

    add react slider

--- react-ecommerce/src/pages/Home.jsx
+++ react-ecommerce/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
+import Announcement from "../components/Announcement.jsx";
+import Slider from "../components/Slider.jsx";
 
 const Home = () => {
   return (
     <div>
+      <Announcement />
       <Navbar />
+      <Slider />
     </div>
   );
 };
