f47dbdfd61132fd5b9d478f776f599a24a46902f
epinark <122348695+epinark@users.noreply.github.com>
2024-01-03 11:24:46 +0100

    add login and register pages

--- react-ecommerce/src/App.jsx
+++ react-ecommerce/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
